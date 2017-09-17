106016f28d114dbb713e521226488dbc63b7dc80
Joram Teusink <joram.teusink@gmail.com>
2017-09-17 16:43:56 +0200

    Update pi-update.sh

--- pi-update.sh
+++ pi-update.sh
@@ -1,33 +1,33 @@
 #!/bin/sh
-echo Initiating apt update and upgrade...
-date
-echo ------------------------------------
+echo To: your_account_name@domain.tld
+echo From: your_account_name@domain.tld
+echo "Subject: Raspberry Pi Update-log: $(date)"
+echo "** Raspberry Pi Update-log: $(date) **"
+echo
+echo * Initiating apt update and upgrade *
+echo -------------------------------------
 sudo apt-get update && sudo apt-get upgrade -y
-echo ------------------------------------
+echo -------------------------------------
 echo
-echo Initiating Raspberry PI update......
-date
-echo ------------------------------------
+echo * Initiating Raspberry PI update... *
+echo -------------------------------------
 sudo rpi-update
-echo ------------------------------------
+echo -------------------------------------
 echo
-echo Initiating apt autoremove...........
-date
-echo ------------------------------------
+echo * Initiating apt autoremove........ *
+echo -------------------------------------
 sudo apt-get autoremove -y
-echo ------------------------------------
+echo -------------------------------------
 echo
-echo Initiating apt autoclean............
-date
-echo ------------------------------------
+echo * Initiating apt autoclean......... *
+echo -------------------------------------
 sudo apt-get autoclean -y
-echo ------------------------------------
+echo -------------------------------------
 echo
-echo Initiating Pi-hole update...........
-date
-echo ------------------------------------
+echo * Initiating Pi-hole update........ *
+echo -------------------------------------
 sudo pihole -up -g
-echo ------------------------------------
+echo -------------------------------------
 echo
-echo Initiating reboot of Raspberry Pi...
+echo "Initiating reboot @ $(date)"
 sudo reboot
