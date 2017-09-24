ecaa5728807292e84610d9a2fccedd3359824c62
Joram Teusink <joram.teusink@gmail.com>
2017-09-24 12:27:19 +0200

    Update pi-update.sh
    Due to the asterisk (*) folder content was shown. Simplified script now.

--- pi-update.sh
+++ pi-update.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
-echo To: your_account_name@domain.tld
-echo From: your_account_name@domain.tld
+echo To: jteusink@xs4all.nl
+echo From: jteusink@xs4all.nl
 echo "Subject: Raspberry Pi Update-log: $(date)"
 echo
-echo "** Raspberry Pi Update-log: $(date) **"
+echo "Raspberry Pi Update-log: $(date)"
 echo
-echo * Initiating apt update and upgrade *
+echo
+echo Initiating apt update and upgrade....
 echo -------------------------------------
 sudo apt-get update && sudo apt-get upgrade -y
 echo -------------------------------------
 echo
-echo * Initiating Raspberry PI update... *
+echo Initiating Raspberry PI update.......
 echo -------------------------------------
 sudo rpi-update
 echo -------------------------------------
 echo
-echo * Initiating apt autoremove........ *
+echo Initiating apt autoremove............
 echo -------------------------------------
 sudo apt-get autoremove -y
 echo -------------------------------------
 echo
-echo * Initiating apt autoclean......... *
+echo Initiating apt autoclean.............
 echo -------------------------------------
 sudo apt-get autoclean -y
 echo -------------------------------------
 echo
-echo * Initiating Pi-hole update........ *
+echo Initiating Pi-hole update............
 echo -------------------------------------
 sudo pihole -up -g
 echo -------------------------------------
