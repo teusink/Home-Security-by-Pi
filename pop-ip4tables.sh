f2ed75e297826ab1551517330c2354e6fccc5474
Joram Teusink <joram.teusink@gmail.com>
2017-11-28 14:35:17 +0100

    Update pop-ip4tables.sh

--- pop-ip4tables.sh
+++ pop-ip4tables.sh
@@ -78,4 +78,4 @@ echo -------------------------
 echo
 echo Now test the functionality of your Pi-Hole yourself!
 echo - If anything is faulty, just restart your Pi.
-echo - If all looks well, run 'sudo iptables-save > /etc/pihole/rules.v4'.
+echo - If all looks well, run 'sudo iptables-save'.
