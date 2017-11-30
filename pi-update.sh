a4e27f9169c3abed04aecc5889316bbf69e70276
Joram Teusink <joram.teusink@gmail.com>
2017-11-30 20:31:45 +0100

    Create pi-update.sh

---
+++ pi-update.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+echo To: <your_account_name>@domain.tld
+echo From: <your_account_name>@domain.tld
+echo "Subject: Raspberry Pi Update-log: $(date)"
+echo
+echo "Raspberry Pi Update-log: $(date)"
+echo
+echo
+echo Initiating apt update and upgrade....
+echo -------------------------------------
+sudo apt-get update && sudo apt-get upgrade -y
+echo -------------------------------------
+echo
+echo Initiating Raspberry PI update.......
+echo -------------------------------------
+sudo rpi-update
+echo -------------------------------------
+echo
+echo Initiating apt autoremove............
+echo -------------------------------------
+sudo apt-get autoremove -y
+echo -------------------------------------
+echo
+echo Initiating apt autoclean.............
+echo -------------------------------------
+sudo apt-get autoclean -y
+echo -------------------------------------
+echo
+echo Initiating Pi-hole update............
+echo -------------------------------------
+sudo pihole -up -g
+echo -------------------------------------
+echo
+echo "Initiating reboot @ $(date)"
+sudo reboot
