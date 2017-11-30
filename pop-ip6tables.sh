8d4ffad195eb26ef443d6dd5631d8e7012551fdc
Joram Teusink <joram.teusink@gmail.com>
2017-11-30 19:18:56 +0100

    Update pop-ip6tables.sh

--- pop-ip6tables.sh
+++ pop-ip6tables.sh
@@ -72,6 +72,7 @@ ip6tables -A OUTPUT -p tcp --dport 1194 -j ACCEPT
 ip6tables -A OUTPUT -p udp --dport 1194 -j ACCEPT
 
 # Allow outgoing SMTP-over-TLS (LAN)
+ip6tables -A OUTPUT -o eth0 -p tcp --dport 465 -j ACCEPT
 ip6tables -A OUTPUT -o eth0 -p tcp --dport 587 -j ACCEPT
 
 # Allow outgoing HTTPS (LAN)
