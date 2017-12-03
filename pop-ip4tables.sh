d764b7cdee75f70c377bc2b2c80acb09ec645e1b
Joram Teusink <joram.teusink@gmail.com>
2017-12-03 14:35:14 +0100

    Update pop-ip4tables.sh

--- pop-ip4tables.sh
+++ pop-ip4tables.sh
@@ -8,18 +8,25 @@
 iptables -F
 iptables -X
 
-# Drop anything else
+## DEFAULT MODUS OPERANDI
+
+# Drop everything (we are going for whitelisting)
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
+
+# Drop Invalid Packets
+iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+iptables -A OUTPUT -m conntrack --ctstate INVALID -j DROP
 
 # Accept all already established connections
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 
-# Drop Invalid Packets
-iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
-iptables -A OUTPUT -m conntrack --ctstate INVALID -j DROP
+# Block incoming HTTPS advertisement assets (anywhere)
+iptables -A INPUT -p tcp --dport 443 -j REJECT --reject-with tcp-reset
+
+## REQUIRED FOR SYSTEM
 
 # Forward VPN Traffic
 iptables -A FORWARD -i tun0 -j ACCEPT
@@ -29,61 +36,65 @@ iptables -A FORWARD -o tun0 -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
-# Allow PING (all)
+# Allow PING
 iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
 
-# Block incoming HTTPS advertisement assets (anywhere)
-iptables -A INPUT -p tcp --dport 443 -j REJECT --reject-with tcp-reset
+## REQUIRED FOR SERVICES DELIVERED BY PI
 
-# Allow DHCP (all)
+# Allow DHCP - incoming & outgoing
 iptables -A INPUT -p udp --dport 67 -j ACCEPT
 iptables -A INPUT -p udp --dport 68 -j ACCEPT
 iptables -A OUTPUT -p udp --dport 67 -j ACCEPT
 iptables -A OUTPUT -p udp --dport 68 -j ACCEPT
 
-# Allow DNS (all)
+# Allow DNS - incoming & outgoing
 iptables -A INPUT -p tcp --dport 53 -j ACCEPT
 iptables -A INPUT -p udp --dport 53 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
 iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 
-# Allow outgoing NTP (all)
+# Allow NTP - outgoing
 iptables -A OUTPUT -p udp --dport 123 -j ACCEPT
 
-# Allow HTTP (all)
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -A INPUT -p udp --dport 80 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
-iptables -A OUTPUT -p udp --dport 80 -j ACCEPT
-
-# Allow incoming and outgoing VNC (all)
-iptables -A INPUT -p tcp -m multiport --dport 5900:5903 -j ACCEPT
-iptables -A OUTPUT -p tcp -m multiport --dport 5900:5903 -j ACCEPT
-
-# Allow incoming and outgoing SSH (all)
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
-
-# Allow incoming and outgoing OpenVPN (all)
+# Allow OpenVPN - incoming & outgoing
 iptables -A INPUT -p tcp --dport 1194 -j ACCEPT
 iptables -A INPUT -p udp --dport 1194 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 1194 -j ACCEPT
 iptables -A OUTPUT -p udp --dport 1194 -j ACCEPT
 
-# Allow outgoing SMTP-over-TLS (LAN)
-iptables -A OUTPUT -o eth0 -p tcp --dport 465 -j ACCEPT
-iptables -A OUTPUT -o eth0 -p tcp --dport 587 -j ACCEPT
+# Allow HTTP - incoming
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p udp --dport 80 -j ACCEPT
 
-# Allow outgoing HTTPS (LAN)
+# Allow VNC - incoming & outgoing
+iptables -A INPUT -p tcp -m multiport --dport 5900:5903 -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --dport 5900:5903 -j ACCEPT
+
+# Allow SSH - incoming & outgoing
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
+
+## REQUIRED FOR SERVICES NEEDED BY PI
+
+# Allow HTTP (LAN) - outgoing
+iptables -A OUTPUT -o eth0 -p tcp --dport 80 -j ACCEPT
+iptables -A OUTPUT -o eth0 -p udp --dport 80 -j ACCEPT
+
+# Allow HTTPS (LAN) - outgoing
 iptables -A OUTPUT -o eth0 -p tcp --dport 443 -j ACCEPT
 iptables -A OUTPUT -o eth0 -p udp --dport 443 -j ACCEPT
 
-# Allow outgoing (s)FTP (LAN)
+# Allow SMTP-over-TLS (LAN) - outgoing
+iptables -A OUTPUT -o eth0 -p tcp --dport 465 -j ACCEPT
+iptables -A OUTPUT -o eth0 -p tcp --dport 587 -j ACCEPT
+
+# Allow (s)FTP (LAN) - outgoing
 iptables -A OUTPUT -o eth0 -p tcp --dport 21 -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --dport 22 -j ACCEPT
 
-# Test fase
+## TEST FASE
+
 echo New iptables rule-set is:
 echo -------------------------
 iptables -L --line-numbers
