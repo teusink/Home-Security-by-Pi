5c359cae35d17932366b884cb1fbe27e868120ad
Joram Teusink <joram.teusink@gmail.com>
2017-12-18 19:33:37 +0100

    Update pi-security-scan.sh

--- pi-security-scan.sh
+++ pi-security-scan.sh
@@ -6,29 +6,34 @@ echo
 echo "Raspberry Pi Anti-mwalware and -rootkit-log: $(date)"
 echo
 echo
-echo Initiating clamfresh database update.
-echo -------------------------------------
+echo ✓ Initiating clamfresh database update.
+echo ---------------------------------------
 sudo /etc/init.d/clamav-freshclam stop
 sudo freshclam
 sudo /etc/init.d/clamav-freshclam start
+echo ---------------------------------------
 echo
-echo Initiating rkhunter database update..
-echo -------------------------------------
+echo ✓ Initiating rkhunter database update..
+echo ---------------------------------------
 sudo rkhunter --update && sudo rkhunter --propupd
+echo ---------------------------------------
 echo
 if [ "$1" = "no-scan" ]
 then
-  echo "Skipping scans @ $(date)"
+  echo "✗ Skipping scans @ $(date)"
 else
-  echo "Initiating Rootkit Hunter scan @ $(date)"
-  echo -------------------------------------
+  echo "✓ Initiating Rootkit Hunter scan @ $(date)"
+  echo ---------------------------------------
   nice -n 19 sudo rkhunter --check --sk --rwo --enable all
+  echo ---------------------------------------  
   echo
-  echo "Initiating chkrootkit scan @ $(date)"
-  echo -------------------------------------
+  echo "✓ Initiating chkrootkit scan @ $(date)"
+  echo ---------------------------------------
   nice -n 19 sudo chkrootkit -q
+  echo ---------------------------------------
   echo
-  echo "Initiating ClamAV scan @ $(date)"
-  echo -------------------------------------
+  echo "✓ Initiating ClamAV scan @ $(date)"
+  echo ---------------------------------------
   nice -n 19 sudo clamscan -r -i --exclude-dir="^/sys" /
+  echo ---------------------------------------
 fi
